9a35bb1710cada3d21924cc3ddeeec8e391c4fae
Josh Brandt <jcbrandt@mit.edu>
2025-09-05 13:51:34 -0400

    updates

--- test/convert_data_file.py
+++ test/convert_data_file.py
@@ -4,20 +4,23 @@ import pandas as pd
 
 data = open("/Volumes/NO_NAME/LOG.DAT", "rb").read()
 
-length_of_bytes = 38
-i = 0
+struct_format = "<fffHffffff"
+
+length_of_struct = struct.calcsize(struct_format)
 
 data_storage = pd.DataFrame(columns = ["temperature_reading", "phototransistor_reading", "IR_sensor_reading", "seconds_now", "accelerometer_x", "accelerometer_y", "accelerometer_z", "magnetometer_x", "magnetometer_y", "magnetometer_z"])
+
+i = 0
 
 while True:
 
     try:
-        data_point = struct.unpack("<fffHffffff", data[i:i+length_of_bytes])
+        data_point = struct.unpack(struct_format, data[i : i + length_of_struct])
     except struct.error:
         break
 
     data_storage.loc[len(data_storage)] = data_point
 
-    i += length_of_bytes
+    i += length_of_struct
 
 data_storage.to_csv("/Volumes/NO_NAME/LOG.csv", index = False)
